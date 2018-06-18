af20a986cc95e1be18739a2df197f5f593659e9c
Rohan995 <39547084+Rohan995@users.noreply.github.com>
2018-06-18 16:37:36 +0530

    Update Jump.sh

--- Jump.sh
+++ Jump.sh
@@ -6,5 +6,5 @@ mv Problem1.sh
 id=$(aws autoscaling describe-auto-scaling-instances --query "AutoScalingInstances[?AutoScalingGroupName=='shellscripting'].InstanceId" --output text) 
 ip=$(aws ec2 describe-instances --instance-ids $id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text )
 scp production.sh ec2-user@$ip:/home/ec2-user
-scp Problem2.sh ec2-user@$ip:/home/ec2-user
+scp Problem1.sh ec2-user@$ip:/home/ec2-user
 ssh ec2-user@$ip /home/ec2-user/production.sh
